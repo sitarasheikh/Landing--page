8055b87917623eb5d0c515b04dbcc662798a4fd5
SItara Sheikh <sitarasheikh89@gmail.com>
2024-10-10 18:53:23 +0500

    Created a landing page using next js and Css for styling

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}', // Adjust based on your project structure
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
